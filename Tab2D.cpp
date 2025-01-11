e299683f930e933e01bd96c73a112070383d4889
ok1000500100900 <kaminskaoliwia32@gmail.com>
2025-01-11 11:02:40 +0100

    Komentarze

--- Tab2D.cpp
+++ Tab2D.cpp
@@ -3,19 +3,24 @@
 #include <time.h>
 
 #define ROWS 4
-#define COLS 5
+#define COLS 4
 
 void initTab2D(int t[][COLS], int nRows, int nCols);
 void printTab2D(int t[][COLS], int nRows, int nCols);
+void Trans(int t[][COLS], int nRows, int nCols);
 
 int main()
 {
   int t[ROWS][COLS];
   initTab2D(t, ROWS, COLS);
   printTab2D(t, ROWS, COLS);
+  Trans(t,ROWS,COLS);
+  printf("\n");
+  printTab2D(t,ROWS,COLS);
   return 0;
 }
 
+// inicjowanie pseudolosowej tablicy
 void initTab2D(int t[][COLS], int nRows, int nCols)
 {
   srand((unsigned)time(NULL));
@@ -25,6 +30,7 @@ void initTab2D(int t[][COLS], int nRows, int nCols)
 
 }
 
+//wydruk tablicy
 void printTab2D(int t[][COLS], int nRows, int nCols)
 {
   for(int i = 0;i < nRows;i++)
@@ -34,6 +40,7 @@ void printTab2D(int t[][COLS], int nRows, int nCols)
     }
 }
 
+// transponowanie macierzy
 void Trans(int t[][COLS], int nRows, int nCols)
 {
   if(nRows != nCols) {
